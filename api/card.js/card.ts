3fe10e7a5f8198bd93bfefb87346de837c1a2e6d
Chloe Cook <63439160+cherryblossom8915@users.noreply.github.com>
2022-01-07 03:19:41 -0500

    Create card.ts

---
+++ api/card.js/card.ts
@@ -0,0 +1,5 @@
+const urlParams = new URLSearchParams(window.location.search);
+const userName = urlParams.get('user');
+const avatar256 = urlParams.get('avatar');
+const guildCount = urlParams.get('count');
+
