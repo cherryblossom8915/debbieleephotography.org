19c7c9ea1325b5a7cdfcf40cba0c231cd14ed243
Chloe Cook <63439160+cherryblossom8915@users.noreply.github.com>
2022-01-14 20:08:29 -0500

    Update index.js

--- api/card/index.js
+++ api/card/index.js
@@ -1,12 +1,7 @@
 function card() {
   const urlParams = new URLSearchParams(window.location.search);
   const userName = urlParams.get('user');
-  const disc = urlParams.get('disc');
   const avatar256 = urlParams.get('avatar');
   const guildCount = urlParams.get('count');
-  console.log(urlParams);
-  console.log(userName);
-  console.log(avatar256);
-  console.log(guildCount);
   return avatar256;
 }
