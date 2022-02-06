23cd6fc1da1ba4f0e8ac8ed46c5ff6a9142c474e
Chloe Cook <63439160+cherryblossom8915@users.noreply.github.com>
2022-02-06 07:26:26 -0500

    Delete api directory

--- api/card/index.js
+++
@@ -1,7 +0,0 @@
-function card() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const userName = urlParams.get('user');
-  const avatar256 = urlParams.get('avatar');
-  const guildCount = urlParams.get('count');
-  return avatar256;
-}
